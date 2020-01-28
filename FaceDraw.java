b1ebe8db6dadf3b8f00b9d79551cfa4bb6f5061e
Chris Dziedzic <krzysztofdziedzic@lewisu.edu>
2020-01-28 15:38:43 -0600

    All requirements except custom feature fulfilled

--- FaceDraw.java
+++ FaceDraw.java
@@ -1,7 +1,4 @@
 import java.awt.Graphics;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -17,30 +14,30 @@ import javax.swing.JPanel;
  */
 public class FaceDraw {
     public static void main(String[] args) {
+        int frameWidth = 1200;
+        int frameHeight = 800;
+        int margin = 30;
         List<Face> FaceList = new ArrayList<>();
         Random random = new Random();
         int count = random.nextInt(8) + 3;
         for (; count > 0; count --) {
-            FaceList.add(new Face());
+            FaceList.add(new Face(frameWidth - margin, frameHeight - margin));
         }
 
-        FaceFrame faceFrame = new FaceFrame(FaceList);
+        FaceFrame faceFrame = new FaceFrame(frameWidth, frameHeight, FaceList);
     }
 }
 
 class Face extends Oval {
     private int smiling;    // -1 frowning, 0 neutral, 1 smiling
-    private int minSize = 50;
 
     private Oval leftEye;
     private Oval rightEye;
-    private JComponent mouth;
+    private Arc mouth;
 
     public Face() {
-        // Unknown frame dimensions... Assuming frame is half the screen height and width:
-        Rectangle bounds = GraphicsEnvironment.getLocalGraphicsEnvironment()
-            .getDefaultScreenDevice().getDefaultConfiguration().getBounds();
-        setRandomDimensions(bounds.width, bounds.height);
+        // Unknown frame dimensions... Assuming 300 by 200:
+        setRandomDimensions(300, 200);
     }
 
     public Face(int frameWidthIn, int frameHeightIn) {
@@ -55,29 +52,103 @@ class Face extends Oval {
         setWidth(widthIn);
         setHeight(heightIn);
         setSmiling(smilingIn);
+        setEyesAndMouth(widthIn, heightIn);
     }
 
     private void setRandomDimensions(int frameWidthIn, int frameHeightIn) {
-        int width = getRandomValue(minSize, frameWidthIn);
-        int height = getRandomValue(minSize, frameHeightIn);
+        // Use the least of the frame's dimensions, so that ovals don't have the tendency 
+        // to stretch horizontally or vertically in correlation with on the screen/frame size
+        int minFrameDimension = (Math.min(frameWidthIn, frameHeightIn));
+        // Large faces tend to overlap a lot. Limit the max size to a fraction of the frame dimensions:
+        minFrameDimension = (int) (minFrameDimension * 0.5);
+        // Prevent the face from being too thin or too wide in relation to its height
+        int minFaceDimension = (int) (minFrameDimension * 0.3);
+        int width = getRandomValue(minFaceDimension, minFrameDimension);
+        int height = getRandomValue(minFaceDimension, minFrameDimension);
         setWidth(width);
         setHeight(height);
         setPositionX(getRandomValue(0, frameWidthIn - width));
         setPositionY(getRandomValue(0, frameHeightIn - height));
         setSmiling(getRandomValue(0, 3) - 1);
+
+        // Add eyes and mouth
+        setEyesAndMouth(width, height);
+    }
+
+    private void setEyesAndMouth(int width, int height) {
+        int x = getPositionX() + (width / 2 - 10 * (width / 60));
+        int y = getPositionY() + (height / 2 - height / 6);
+        int w = width / 10;
+        int h = height / 4;
+
+        leftEye = new Oval(x, y, w, h);
+
+        x = getPositionX() + (width / 2 + 4 * (width / 60));
+        rightEye = new Oval(x, y, w, h);
+
+        x = getPositionX() + width / 4;
+        y = getPositionY() + (int) (height * 0.7);
+        w = (int) (width / 2);
+        h = (int) (height * 0.2);
+        mouth = new Arc(x, y, w, h, smiling);
     }
 
     private int getRandomValue(int min, int max) {
         if (min >= max || min < 0 || max < 0) {
             throw new IllegalArgumentException("The value of min must be less " +
-                "than the value of max and both of them must be positive integers");
+                "than the value of max and both of them must be positive integers. " +
+                "Only min may be 0");
         }
         Random random = new Random();
-        int result = random.nextInt(max);
-        while (result < min) {
-            result = random.nextInt(max);
+        return random.nextInt(max - min) + min;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawOval(getPositionX(), getPositionY(), getWidth(), getHeight());
+        leftEye.paintComponent(g);
+        rightEye.paintComponent(g);
+        mouth.paintComponent(g);
+    }
+
+    @Override
+    public String toString() {
+        return(String.format("Face: x=%d, y=%d, width=%d, height=%d, smiling=%s",
+            getPositionX(), getPositionY(), getWidth(), getHeight(),
+            (smiling < 0 ? "Frowning" : (smiling > 0 ? "Smiling" : "Neutral"))));
+    }
+
+    public void setSmiling(int smilingIn) {
+        smiling = smilingIn;
+        if (mouth != null) {
+            mouth.setSmiling(smilingIn);
         }
-        return result;
+    }
+    public int getSmiling() { return smiling; }
+}
+
+class Arc extends Oval {
+    private int smiling = 0;
+
+    public Arc(int positionXIn, int positionYIn, int widthIn, int heightIn, int smilingIn) {
+        setPositionX(positionXIn);
+        setPositionY(positionYIn);
+        setWidth(widthIn);
+        setHeight(heightIn);
+        setSmiling(smilingIn);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        // super.paintComponent(g);
+        if (smiling < 0) {
+            g.drawArc(getPositionX(), getPositionY() - getHeight(), getWidth(), getHeight(), 190, 160);
+        } else if (smiling == 0) {
+            g.drawLine(getPositionX(), getPositionY(), getPositionX() + getWidth(), getPositionY());
+        } else {
+            g.drawArc(getPositionX(), getPositionY(), getWidth(), getHeight(), 10, 160);
+        }
     }
 
     public void setSmiling(int smilingIn) { smiling = smilingIn; }
@@ -90,19 +161,30 @@ class Oval extends JComponent {
     private int height;
     private int width;
 
+    public Oval() {
+
+    }
+
+    public Oval(int positionXIn, int positionYIn, int widthIn, int heightIn) {
+        setPositionX(positionXIn);
+        setPositionY(positionYIn);
+        setWidth(widthIn);
+        setHeight(heightIn);
+    }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawOval(positionX, positionY, width, height);
+        g.drawOval(getPositionX(), getPositionY(), getWidth(), getHeight());
     }
 
-    public void setPositionX(int x) { positionX = x; }
+    public void setPositionX(int positionXIn) { positionX = positionXIn; }
     public int getPositionX() { return positionX; }
-    public void setPositionY(int y) { positionY = y; }
+    public void setPositionY(int positionYIn) { positionY = positionYIn; }
     public int getPositionY() { return positionY; }
-    public void setHeight(int height) { this.height = height; }
+    public void setHeight(int heightIn) { height = heightIn; }
     public int getHeight() { return height; }
-    public void setWidth(int width) { this.width = width; }
+    public void setWidth(int widthIn) { width = widthIn; }
     public int getWidth() { return width; }
 }
 
@@ -126,20 +208,21 @@ class FacePanel extends JPanel {
 
     @Override
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        getFaces().forEach(e -> { e.paintComponent(g); });
+        getFaces().forEach(e -> { 
+            e.paintComponent(g);
+            System.out.println(e);
+         });
     }
 }
 
 class FaceFrame extends JFrame {
     private JPanel facePanel;
-    public FaceFrame(List<Face> faces) {
+    public FaceFrame(int widthIn, int heightIn, List<Face> FaceList) {
         super("FaceDraw");
-        Rectangle bounds = GraphicsEnvironment.getLocalGraphicsEnvironment()
-            .getDefaultScreenDevice().getDefaultConfiguration().getBounds();
-        setBounds(bounds);
+        setSize(widthIn, heightIn);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        facePanel = new FacePanel(faces);
+
+        facePanel = new FacePanel(FaceList);
         setContentPane(facePanel);
         setVisible(true);
     }
